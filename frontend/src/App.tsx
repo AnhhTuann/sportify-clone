5b25fae55703e2e53a320563d768e9d9bef6b842
AnhhTuann <tuannguyen25101999@gmail.com>
2024-11-08 16:41:23 +0700

    init new frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Hello</h1>
+      <Button variant={"outline"}>Click me</Button>
+    </>
+  );
+}
+
+export default App;
